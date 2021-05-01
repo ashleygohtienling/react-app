ecd8d4a8314fd246abc4183f3b3b4238f7aca7f2
ashleygohtienling <83412042+ashleygohtienling@users.noreply.github.com>
2021-05-01 21:34:15 +0800

    Created Login Page

--- src/App.js
+++ src/App.js
@@ -12,35 +12,39 @@ import Footer from './Components/Footer.js'
 import Home from './Views/Home'
 import About from './Views/About'
 import Product from './Views/Product'
-
+import Signup from './Views/Signup'
+import { Container } from "react-bootstrap"
 
 function App() {
   return (
-       <div>  
-          
+       <div> 
           <Router> 
-          
           <Header/>
           <div className= "p-3">
-
+          <Container className="d-flex align-items-center justify-content-center"
+              style = {{minHeight: "80vh"}} >
           <Switch>
+          <div className= "w-50" style = {{maxWeidth: '400px'}} > 
               <Route exact path="/"> 
-                <Home />
-              </Route>
+                <Signup />
+                </Route>
+          </div>
+             
               <Route path="/about"> 
                 <About />
               </Route>
               <Route path="/products/:id">
               <Product />
-              </Route>
-             
 
+              </Route>
               </Switch>
+              </Container>
+          
           </div>
             
               <Footer/>
           </Router>
-         
+        
        </div>
 
   )
